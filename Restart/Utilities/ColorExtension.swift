ab1506888a16745dece028d6e4f2953b2d575e61
Amir Vahedi <amirvahedix@gmail.com>
2024-04-17 17:21:01 +0330

    Created OnboardingView

---
+++ Restart/Utilities/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  Restart
+//
+//  Created by Amir on 1/29/1403 AP.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBlue = Color("ColorBlue")
+    static let customRed = Color("ColorRed")
+}
