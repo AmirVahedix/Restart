1d6963fdc5cb3adaf0e41a97510f5a78bbdca490
Amir Vahedi <amirvahedix@gmail.com>
2024-04-17 20:40:09 +0330

    Designed HomeView, Implemented CircleGroupView

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Amir on 1/29/1403 AP.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
