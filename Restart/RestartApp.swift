380a47ca8f64dadf997d0d7c172726f5fd957ff6
Amir Vahedi <amirvahedix@gmail.com>
2024-04-17 16:31:16 +0330

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Amir on 1/29/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
